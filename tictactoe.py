89372e2ee59c7fa6be1ae268fe86f69058b56fe0
Meshack Kipkorir <meshackkipkorir105@gmail.com>
2020-06-18 11:34:30 +0300

    implemnted function to check for winner

--- tictactoe.py
+++ tictactoe.py
@@ -16,6 +16,16 @@ def isBoardFull(board):
     else:
         return True
     
+#check for winner,checks for rows,diagonals,columns if they have the same letter
+def isWinner(board,letter):
+    return ((board[1] == board[2] == board[3] == letter) or
+            (board[4] == board[5] == board[6] == letter) or
+            (board[7] == board[8] == board[9] == letter) or
+            (board[1] == board[4] == board[7] == letter) or
+            (board[2] == board[5] == board[8] == letter) or
+            (board[3] == board[6] == board[9] == letter) or
+            (board[1] == board[5] == board[9] == letter) or
+            (board[3] == board[5] == board[7] == letter))
     
 
 
