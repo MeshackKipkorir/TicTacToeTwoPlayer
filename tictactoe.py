9aaec2bb5eeefff344b00e628e66a90e154e3b1b
Meshack Kipkorir <meshackkipkorir105@gmail.com>
2020-06-18 12:13:25 +0300

    implemented player two move

--- tictactoe.py
+++ tictactoe.py
@@ -26,8 +26,9 @@ def isWinner(board,letter):
             (board[3] == board[6] == board[9] == letter) or
             (board[1] == board[5] == board[9] == letter) or
             (board[3] == board[5] == board[7] == letter))
-    
- def playerOneMove():
+
+#player one move
+def playerOneMove():
     print('****Player One Turn****')
     run = True 
     while run:
@@ -44,6 +45,35 @@ def isWinner(board,letter):
                 print('Enter a number between 1 and 9')
         except:
             print('Enter a number')
+            
+ #player two move
+ 
+def main():
+    print('*****Welcome to the game !!!')
+    printBoard(board)
 
+    while not (isBoardFull(board)):
+        if not (isWinner(board, 'O')):
+            playerOneMove()
+            printBoard(board)
+        else:
+            print('Player Two Won !')
+            break
+
+        if not (isWinner(board, 'X')):
+            playertwoMove()
+            printBoard(board)
+        else:
+            print('Player One Won')
+            break
+
+        if isBoardFull(board):
+            print('Tie game !')
+
+
+
+            
+            
+   
 
    
